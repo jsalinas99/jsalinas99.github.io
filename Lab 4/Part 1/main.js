ded277bcee0850678b71aa05d77ee1efea939827
jsalinas99 <jasper.salinas@dcmail.ca>
2024-11-21 00:41:36 -0500

    constant/placeholders added in main.js

--- Lab 4/Part 1/main.js
+++ Lab 4/Part 1/main.js
@@ -15,16 +15,25 @@ let storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. Whe
 
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+//3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
 function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
 
   }
 
