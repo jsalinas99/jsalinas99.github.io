29a7c4ca58e0906858a3ad089c604d40278f44f2
jsalinas99 <jasper.salinas@dcmail.ca>
2024-11-22 17:22:44 -0500

    Update main-finished.js

--- Lab 4/Part 4/main-finished.js
+++ Lab 4/Part 4/main-finished.js
@@ -1,4 +1,6 @@
 // set up canvas
+const para = document.querySelector('p');
+let count = 0;
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -163,17 +165,27 @@ while (balls.length < 25) {
    );
 
    balls.push(ball);
+   count++;
+   para.textContent = 'Ball count: ' + count;
 }
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0, width, height);
 
    for (const ball of balls) {
-      ball.draw();
-      ball.update();
-      ball.collisionDetect();
-   }
+      if (ball.exists) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+      }
+    }
+
+   evilBall.draw();
+   evilBall.checkBounds();
+   evilBall.collisionDetect();
 
    requestAnimationFrame(loop);
 }
