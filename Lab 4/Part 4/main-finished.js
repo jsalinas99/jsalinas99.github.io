be9f95ea7f8b43a3e0680a49e49b4a3670ab24ae
jsalinas99 <jasper.salinas@dcmail.ca>
2024-11-22 17:03:32 -0500

    Update main-finished.js

--- Lab 4/Part 4/main-finished.js
+++ Lab 4/Part 4/main-finished.js
@@ -130,6 +130,19 @@ class EvilCircle extends Shape {
    }
 
    collisionDetect() {
+      for (const ball of balls) {
+         if (ball.exists) {
+           const dx = this.x - ball.x;
+           const dy = this.y - ball.y;
+           const distance = Math.sqrt(dx * dx + dy * dy);
+   
+           if (distance < this.size + ball.size) {
+             ball.exists = false;
+             count--;
+             para.textContent = 'Ball count: ' + count;
+           }
+         }
+       }
 
    }
 }
